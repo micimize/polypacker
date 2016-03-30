4002bfce9c0d91490165fae6cd0fb555b31465df
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2016-03-30 14:52:47 -0500

    rebranded to polypacker

--- webpack-config.js
+++ webpack-config.js
@@ -10,14 +10,21 @@ module.exports = function(options){
         modules    = options.modules,
         react    = options.react,
         es6Modules = options.es6Modules;
+        context = options.context;
         includeNodeModules = options.includeNodeModules || [];
     process.chdir(process.env.PWD)
     var pwd = './'
     var config =  {
+      devtool: 'source-map',
       externals: [ nodeExternals({ whitelist: includeNodeModules }) ],
       plugins: [
+		new webpack.DefinePlugin({ $ES: { CONTEXT: JSON.stringify(context) || JSON.stringify('NODE'), ENV: JSON.stringify('PRODUCTION')} }),
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.BannerPlugin(
+          'require("source-map-support").install();',
+          { raw: true, entryOnly: false }
+        ),
         new webpack.optimize.UglifyJsPlugin(),
 		new webpack.NoErrorsPlugin(),
       ],
@@ -35,16 +42,33 @@ module.exports = function(options){
       module: {
         loaders: [
           {
-            test: /\.js$/,
-            loaders: [ 'babel?presets[]=stage-0&presets[]=es2015' + (react ? '&presets[]=react' : '')],
+            test: /\.js|\.jsx$/,
+            loaders: [ 'babel?presets[]=es2015' + (react ? '&presets[]=react' : '') + '&presets[]=stage-0'],
             exclude: es6Modules ? XRegExp('/node_modules\/(?!' + es6Modules + ')/') : /node_modules/,
             postLoaders: [
             ],
             noParse: /\.min\.js/
-        },
-        {
+          }, {
             test: /\.json$/, loader: 'json'
-        }
+          }, {
+            test: /\.css$/, loader: 'style!css!postcss'
+          }, {
+            test: /\.less$/, loader: 'style!css!less'
+          }, {
+            test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&' + "includePaths[]=" + (path.resolve(process.cwd(), "./node_modules"))
+          }, {
+            test: /\.woff(2)?(\?.+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" 
+          }, {
+            test: /\.ttf(\?.+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" 
+          }, {
+            test: /\.eot(\?.+)?$/, loader: "file" 
+          }, {
+            test: /\.svg(\?.+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"
+          }, {
+            test: /\.png$/, loader: "url-loader?limit=100000" 
+          }, {
+            test: /\.jpg$/, loader: "file-loader" 
+          } 
         ],
       },
       entry: [ server ],
@@ -58,6 +82,7 @@ module.exports = function(options){
       config.devtool = 'source-map'
       config.debug = true
       config.plugins = [
+		new webpack.DefinePlugin({ $ES: { CONTEXT: JSON.stringify(context) || JSON.stringify('NODE'), ENV: JSON.stringify('DEVELOPMENT')} }),
         new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin(),
         new webpack.BannerPlugin(
